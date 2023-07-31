99c488a5b0dcadd21ef42b7b000254ed68710867
Nolan Knill <nolanknill@gmail.com>
2023-07-31 11:47:34 -0400

    Initial starter code for useEffect and axios

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,21 @@
+import { Link, useParams } from "react-router-dom";
+
+function Home() {
+    const params = useParams();
+
+    console.log("This function is running!");
+    console.log("params", params);
+
+    return (
+        <>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/tv-shows/1">TV Show 1</Link></li>
+                <li><Link to="/tv-shows/2">TV Show 2</Link></li>
+            </ul>
+            <h2>TV Show #: {params.id}</h2>
+        </>
+    );
+}
+
+export default Home;
